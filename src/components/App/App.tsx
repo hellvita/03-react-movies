810432eaca065fefe2184e0161af483069d8d66b
hellvita <seredaolga03@gmail.com>
2025-11-25 15:18:22 +0200

    add the fetchMovies function

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,3 +1,12 @@
+// import { fetchMovies } from "../../services/movieService";
+
 export default function App() {
+  // const handleSearch = async (title: string) => {
+  //   try {
+  //     const data = await fetchMovies(title);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
   return <></>;
 }
